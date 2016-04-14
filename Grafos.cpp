1b4910b4b960a6bec5e4fdb82227268996cc45b2
osmirmariano <osmirmariano@uft.edu.br>
2016-04-14 13:42:58 -0300

    Atualização: matriz recebe arquivo, certinho agora

--- Grafos.cpp
+++ Grafos.cpp
@@ -35,7 +35,7 @@ class Grafos{
         txtFile.open("matriz.txt");
 
         while (txtFile.get(elemento)){
-            cout << elemento;
+            //cout << elemento;
             if(elemento == '\n')
                 linha++;
         
@@ -43,28 +43,72 @@ class Grafos{
         txtFile.close();
         cout << endl << endl;
         cout << "QUANTIDADE: " << linha << endl;
-        char matriz[linha][linha];
         
+        int matrix[linha][linha];
+        //std::vector<std::vector<int> > matrix(2*linha, std::vector<int>(2*linha));
+
         ifstream myfile;
         myfile.open("matriz.txt");
-        while (! myfile.eof()){
-            for (int x = 0; x < linha; x++){
-                cout << "" << endl;
-                for (int y = 0; y < linha; y++){
-                    /*if (matriz[i][j] == '[' && matriz[i][j] == ']'){
 
-                    }*/
-                    myfile >> matriz[x][y];
+        //myfile >> matrix[0][0];
 
-                    cout << " X: " << x << " " << matriz[x][y];
-                    cout << " Y: " << y << " " << matriz[x][y];
+
+        int l = 0;
+        
+        while (!myfile.eof()){
+            /*for (int x = 0; x < linha + 2; x++){
+                cout << " " << endl;
+                for (int y = 0; y < linha + 2; y++){
+                    
+                    char c;
+                    myfile >> c;
+
+                    if(c != '[' && c != ']')
+                        matrix[x-1][y-1] = c;
+
+                   //myfile >> matriz[x][y];                    
+                   cout << "  " << matriz[x][y];
                 }
                 myfile.ignore();
-                cout << "" << endl;
+                cout << " " << endl;
+            }*/
+                int c = 0;
+                string str;
+
+                getline(myfile, str);
+
+                for(int i = 0; i < str.size(); ++i)
+                {
+                    if(str[i] == '-')
+                    {
+                        matrix[l][c] = -(str[i+1] - '0');
+                        i++;
+                        c++;
+                        continue;
+                    }
+
+                    else if(str[i] != '[' && str[i] != ']' && str[i] != ' ' && str[i] != '\n')
+                    {
+                        matrix[l][c] = str[i] - '0';
+                        ++c;
+                    }
+                }
+
+                ++l;
             }
+
+        for (int x = 0; x < linha; x++)
+        {
+            for (int y = 0; y < linha; y++)
+            {
+                cout << matrix[x][y] << " ";
+            }
+
+            cout << endl;
         }
+
         myfile.close();
-        cout << "MATRIZ NA POSICÃO 4 E 4: " << matriz[4][3];
+        cout << "MATRIZ NA POSICÃO 4 E 4: " << matrix[1][0];
     };
     
 };
